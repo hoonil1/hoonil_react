a15485b51cdc8ce678278540d1c8e72a740e7b9a
hoonil1 <phi0588@naver.com>
2023-10-31 14:54:34 +0900

    jsx에서 javascript 코드 작성은 {} 사용 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+function MyComp() {
+  const address = "seoul";
+  const country = "korea";
+  const phone = "010~~";
+  const age = 44;
+  const random = Math.random();
+  return (
+    <>
+      <h1>i am {age} years old</h1>
+      <h1>and i live in {country}</h1>
+      <h1>also i live in {address}</h1>
+      <h1>my phonenumber is {phone}</h1>
+      <h1>{random}</h1>
+    </>
+  );
+}
 
 function App() {
   return (
     <div>
-
+      <MyComp />
     </div>
   );
 }
