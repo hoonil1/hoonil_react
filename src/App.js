4b2f5a0fa211a5e7ea3555930a59a7cb714b6937
hoonil1 <phi0588@naver.com>
2023-10-31 14:25:11 +0900

    종료태그 작성연습 완료, 컴포넌트 여러번 사용 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-
 function App() {
   return (
     <div>
+      <MyComponent1 />
+      <MyComponent1 />
+      <MyComponent1 />
+    </div>
+  );
+}
 
+function MyComponent1() {
+  return (
+    <div style={{ width: "100px" }}>
+      <img src="https://i.imgur.com/dgEMfL8.jpeg" alt="MITOMA" />
     </div>
   );
 }
