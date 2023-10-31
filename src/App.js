772bdf5a7fc56397ffebf74f6d87b0a47cbc114a
hoonil1 <phi0588@naver.com>
2023-10-31 15:42:29 +0900

    jsx에서 js키워드 사용불가

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 function App() {
-  // JSX에서 태그 사용시 꼭 종료태그를 작성해야 한다 ex) </div>
-  // 빈 요소이면 시작태그에서 종료를 꼭 해주어야 한다 ex) <br>
-  const random = Math.ceil(Math.random() * 100);
-  const isLarge = random > 50;
+  // jsx가 js코드로 변환됨
+  // 따라서 js 키워드를 사용할수 없다.
   return (
     <>
-      <h1>난수 : {random}</h1>
-      <h1>{random > 50 ? "큰수" : "작은수"}</h1>
-      <div>{random > 50 ? <BigImage /> : <SmallImage />}</div>
-      <div>{isLarge && <h1>큰 수다 !</h1>}</div>
-      <div>{isLarge || <h1>작은 수다 !</h1>}</div>
+      {/* class 속성은 className 으로 작성 */}
+      <div className="header">Lorem ipsum dolor.</div>
+
+      {/*  label 의 for 속성은 htmlFor 로 작성 */}
+      <label htmlFor="nameInput">이름</label>
+      <input type="text" name="nameInput" />
     </>
   );
 }
-
-function BigImage() {
-  return <h1>큰 이미지</h1>;
-}
-
-function SmallImage() {
-  return <h3>작은 이미지</h3>;
-}
-
-// component 이름은 대문자로 시작.
-// built-in component 이름은 소문자로 시작.
-// return 문을 가져야한다 (항상은 아님)
-// return 값은 보통 JSX 코드이다. (항상은 아님)
 
 export default App;
