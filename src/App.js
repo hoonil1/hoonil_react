5db95c70a14f7c4afbecd90a81c9d1c85794de9b
hoonil1 <phi0588@naver.com>
2023-10-31 12:14:43 +0900

    component 연습, 작성완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-
 function App() {
-  return (
-    <div>
+    return (
+        <div>
+            <MyComponent1/>
+            {/*
+      <div>
+      <h1>hello component</h1>
+      </div>
+      */}
+            <MyComponent2/>
+        </div>
+    );
+}
 
-    </div>
-  );
+function MyComponent1() {
+    return (
+        <div>
+            <h1>Hello Component</h1>
+        </div>
+    )
+}
+
+function MyComponent2() {
+    return (
+        <div>
+            <p>Lorem ipsum dolor.</p>
+            <ul>
+                <li>Lorem.</li>
+                <li>Pariatur?</li>
+                <li>Quibusdam.</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
