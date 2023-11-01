486d03f6da2372bb0932ca84560c3f7fec3e9903
hoonil1 <phi0588@naver.com>
2023-11-01 11:47:53 +0900

    코드가 너무 길어질때 component를 다른 파일로 작성해서 export, import 하는법

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
+import { MyElem } from "./Component/MyElem";
+import { MyComp } from "./Component/MyComp";
+
+// 코드가 매우 긴 컴포넌트가 있을때에는
+// 같은 폴더 내에 컴포넌트 이름으로 파일을 새로 만들어주고
+// 다른 파일에서 사용 가능하도록 export 해주어야 한다
+// export 된 컴포넌트(또는 변수,함수,객체)를 import 해서 사용한다.
+
 function App() {
   return (
-    <div>
-      <MyComp
-        m={"30px"}
-        p={"10px"}
-        color={"gold"}
-        bg={"black"}
-        text={"hello react"}
-      />
-      <MyComp m={"30px"} bg={"black"} text={"hello react"} />
-
-      <MyComp p={"10px"} color={"gold"} />
-    </div>
-  );
-}
-
-function MyComp({
-  m = "0 auto",
-  p = "0",
-  color = "white",
-  bg = "green",
-  text = "No Comments",
-}) {
-  return (
-    <div
-      style={{
-        margin: m,
-        padding: p,
-        color: color,
-        background: bg,
-      }}
-    >
-      <p>{text}</p>
-    </div>
+    <>
+      <MyComp />
+      <MyElem />
+    </>
   );
 }
 
---
+++ src/Component/MyElem.js
@@ -0,0 +1,8 @@
+export function MyElem() {
+  return (
+    <div>
+      <p>간단한 컴포넌트</p>
+      <p>매우 긴 코드를 가진 컴포넌트일때</p>
+    </div>
+  );
+}
