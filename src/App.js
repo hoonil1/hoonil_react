8f5b7bb9f328cede9e2ce2a1470dd6cccabc9e6e
hoonil1 <phi0588@naver.com>
2023-11-01 11:22:16 +0900

    props + 구조분해할당 기본값 지정하기

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ function App() {
   // built-in components
   return (
     <>
-      <MyComp value={"흥민"} address={"신촌"} />
-      <MyComp value={"강인"} address={"강남"} />
+      <MyComp name={"흥민"} address={"신촌"} age={33} city={"서울"} />
+      <MyComp address={"신촌"} age={33} city={"서울"} />
     </>
   );
 }
 
 //함수 인자로 사용한 곳에서 넘긴 프로퍼티들이 객체형태로 담긴다
-function MyComp({ value, address }) {
+function MyComp({ name = "NoName", address, city, age = 100 }) {
   return (
     <div>
-      {value}이는 {address}에 산다
+      <p>
+        {name}은 {age}세 이고 {address},{city}에 산다
+      </p>
     </div>
   );
 }
