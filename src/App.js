f565fc97aa0c2708b642e1f569aea2fcdd5ac56c
hoonil1 <phi0588@naver.com>
2023-11-01 09:48:08 +0900

    props 관련공부22

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-
 function App() {
   return (
     <div>
+      <MyHeader color={"white"} bg={"black"} text={"내가만든제목"} />
+      <MyHeader color={"orange"} bg={"skyblue"} text={"다시만든제목"} />
+    </div>
+  );
+}
 
+function MyHeader(props) {
+  return (
+    <div
+      style={{
+        color: props.color,
+        background: props.bg,
+      }}
+    >
+      <h1>text : {props.text}</h1>
     </div>
   );
 }
