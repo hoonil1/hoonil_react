66262851e985429f78b57d56b77cfd7e750d0cd9
hoonil1 <phi0588@naver.com>
2023-11-01 10:46:37 +0900

    props + 구조분해할당 연습공부

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-
 function App() {
   return (
     <div>
+      <MyElem name={"SON"} age={32} team={"Tottenham"} number={7} />
+      <MyElem name={"LEE"} age={22} team={"Paris"} number={19} />
+    </div>
+  );
+}
 
+function MyElem({ name, age, team, number }) {
+  return (
+    <div
+      style={{
+        border: "1px solid black",
+      }}
+    >
+      <p
+        style={{
+          textAlign: "center",
+          fontStyle: "italic",
+        }}
+      >
+        NO.{number} {name}, age {age}, Is In {team}
+      </p>
     </div>
   );
 }
