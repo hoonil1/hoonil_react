12e4df8f5447e35a9014fbf5efb5c8d9ab3e6589
hoonil1 <phi0588@naver.com>
2023-11-01 16:29:17 +0900

    css 모듈을 사용할때 여러 클래스명을 주는 방법

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import MyBox, { address } from "./Component/MyBox";
-import MyElem, { address as myAddress } from "./Component/MyElem";
-
-// as 로 named export 값을 import 할때 별칭을 줄 수 있다.
-// 중복으로 인한 충돌을 피하기 위해서
+import { MyContainer } from "./Component/MyContainer";
 
 function App() {
   // built-in components
   return (
     <>
-      <h1>{address}</h1>
-      <h1>{myAddress}</h1>
+      <MyContainer />
     </>
   );
 }
