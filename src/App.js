23be55735e39a43857172a34dd4e51a7bf2e2198
hoonil1 <phi0588@naver.com>
2023-11-01 14:27:18 +0900

    default, export, import 에 대해  연습완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-
+import MyBox, { person, team } from "./Component/MyContainer";
 function App() {
   return (
     <div>
-
+      <MyBox />
+      <h1>{person.name}</h1>
+      <h1>{person.address}</h1>
+      <h1>{team.title}</h1>
+      <h1>{team.score}</h1>
     </div>
   );
 }
---
+++ src/Component/MyContainer.js
@@ -0,0 +1,12 @@
+function MyContainer() {
+  return (
+    <div>
+      <h1>Today's MVP is</h1>
+    </div>
+  );
+}
+
+export const person = { name: "SON", address: "Seoul" };
+export const team = { title: "Tottenham", score: "1:0" };
+
+export default MyContainer;
