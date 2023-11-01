597f81f739465efc922328199e57b2b081f1a209
hoonil1 <phi0588@naver.com>
2023-11-01 10:31:43 +0900

    props + 구조분해할당 공부

--- src/App.js
+++ src/App.js
@@ -2,27 +2,17 @@ function App() {
   // built-in components
   return (
     <>
-      <h1
-        className={"note"}
-        style={{ background: "blue", color: "white" }}
-        title={"제목요소"}
-      >
-        Lorem ipsum.
-      </h1>
-      <p title={"문단요소"}>Lorem ipsum dolor.</p>
-
-      <MyComp title={"내 컴포넌트"} name={"흥민"} />
-      <MyComp title={"I AM COMPONENT"} name={"KANG IN"} />
+      <MyComp value={"흥민"} address={"신촌"} />
+      <MyComp value={"강인"} address={"강남"} />
     </>
   );
 }
 
 //함수 인자로 사용한 곳에서 넘긴 프로퍼티들이 객체형태로 담긴다
-function MyComp(property) {
-  console.log("MyComp가 받은 프로퍼티", property);
+function MyComp({ value, address }) {
   return (
     <div>
-      <h1 title={property.title}>hello {property.name}</h1>
+      {value}이는 {address}에 산다
     </div>
   );
 }
