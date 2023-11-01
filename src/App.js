914b140ef7358f38a4f98e73531d5c3e2fe9af59
hoonil1 <phi0588@naver.com>
2023-11-01 11:15:07 +0900

    props + 구조분해할당 연습공부22

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-
 function App() {
   return (
     <div>
+      <MyBox
+        p={"10px"}
+        m={"0 auto"}
+        color={"blue"}
+        bg={"yellow"}
+        text={"result1"}
+      />
+      <MyBox
+        p={"50px"}
+        m={"0 auto"}
+        color={"white"}
+        bg={"green"}
+        text={"result2"}
+      />
+    </div>
+  );
+}
 
+function MyBox({ p, m, color, text, bg }) {
+  return (
+    <div
+      style={{
+        padding: p,
+        margin: m,
+        color: color,
+        background: bg,
+      }}
+    >
+      <p>{text}</p>
     </div>
   );
 }
