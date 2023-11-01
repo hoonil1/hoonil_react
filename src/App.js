6d1f34c223f484c2a71ee943aa89fd0bddca8ee9
hoonil1 <phi0588@naver.com>
2023-11-01 16:11:37 +0900

    css 모듈을 사용해서 클래스명 중복을 피함

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+// style 적용 방법
+// 보통 css 파일
+// css module 사용
+// chakraui css library
+
+import { MyComp } from "./component/MyComp";
+import { MyBox } from "./component/MyBox";
+import { MyContainer } from "./component/MyContainer";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <MyComp />
+      <MyBox />
+      <MyContainer />
+    </>
   );
 }
 
---
+++ src/Component/MyContainer.js
@@ -0,0 +1,9 @@
+import style from "../style/style2.module.css";
+
+export function MyContainer() {
+  return (
+    <div>
+      <h1 className={style.boxClass}>Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+}
