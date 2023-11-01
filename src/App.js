a599dc3ba5cb278cb38cbc781fa662f63c38726c
hoonil1 <phi0588@naver.com>
2023-11-01 11:35:55 +0900

    props + 구조분해할당 기본값 지정, 연습, 시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-
 function App() {
   return (
     <div>
+      <MyComp
+        m={"30px"}
+        p={"10px"}
+        color={"gold"}
+        bg={"black"}
+        text={"hello react"}
+      />
+      <MyComp m={"30px"} bg={"black"} text={"hello react"} />
 
+      <MyComp p={"10px"} color={"gold"} />
+    </div>
+  );
+}
+
+function MyComp({
+  m = "0 auto",
+  p = "0",
+  color = "white",
+  bg = "green",
+  text = "No Comments",
+}) {
+  return (
+    <div
+      style={{
+        margin: m,
+        padding: p,
+        color: color,
+        background: bg,
+      }}
+    >
+      <p>{text}</p>
     </div>
   );
 }
