e14122ddd6a8ecd99880f9633411500ec477c9d4
hoonil1 <phi0588@naver.com>
2023-11-02 09:11:53 +0900

    css 모듈을 사용할때 여러 클래스명을 주는 방법 -> template literal 또는 array.join으로 변경

--- src/component/MyText.js
+++ src/component/MyText.js
@@ -1,9 +1,12 @@
-import "../style/mystyle.module.css";
+import css from "../style/mystyle.module.css";
 
 export function MyText() {
   return (
     <div>
       <h1 className={`${css.error} ${css.coffee} ${css.special}`}>
+        Lorem ipsum dolor sit amet.
+      </h1>
+      <h1 className={[css.coffee, css.special, css.error].join(" ")}>
         Lorem ipsum dolor sit amet.
       </h1>
     </div>
