53b797962c6daef2eb58518a7ba4f746932e78fb
hoonil1 <phi0588@naver.com>
2023-11-02 14:38:29 +0900

    Button의 각종 기능 사용

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import { Button, useToast } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { PhoneIcon } from "@chakra-ui/icons";
 
 function App() {
-  const toast = useToast();
   return (
     <>
-      <Button
-        onClick={() =>
-          toast({
-            description: "게시물이 저장되었습니다.",
-            title: "저장완료",
-            duration: 500,
-            position: "center",
-            isClosable: true,
-          })
-        }
-      >
-        토스트 띄우기
+      <Button colorScheme="blue">Lorem.</Button>
+      <Button colorScheme="red">Officia?</Button>
+      <Button colorScheme="yellow">Aliquam!</Button>
+      <Button colorScheme="green">Libero!</Button>
+      <Button colorScheme="orange">Quis.</Button>
+      <Button colorScheme="gray">Perspiciatis.</Button>
+      <Button size={"xs"}>Pariatur!</Button>
+      <Button size={"sm"}>Voluptatum!</Button>
+      <Button size={"md"}>Cupiditate?</Button>
+      <Button size={"lg"}>Sit.</Button>
+      <Button colorScheme="red" variant={"outline"}>
+        Inventore.
       </Button>
-      <Button
-        onClick={() =>
-          toast({
-            description: "게시물이 저장되지 않았습니다.",
-            title: "저장실패",
-            duration: 500,
-            position: "top",
-            isClosable: true,
-            status: "error",
-          })
-        }
-      >
-        토스트 띄우기
+      <Button colorScheme="facebook" variant={"outline"}>
+        Officiis.
       </Button>
+      <Button variant={"ghost"}>Blanditiis.</Button>
+      <Button leftIcon={<PhoneIcon />}>Beatae.</Button>
+      <Button isLoading loadingText={"Connecting"}>
+        Similique!
+      </Button>
+      <Button>Accusantium.</Button>
+      <Button>Repudiandae.</Button>
+      <Button>Obcaecati?</Button>
+      <Button>Neque?</Button>
+      <Button>Enim.</Button>
+      <Button>Veritatis?</Button>
+      <Button>Id.</Button>
+      <Button>Eligendi.</Button>
+      <Button>Sequi?</Button>
+      <Button>Veritatis?</Button>
+      <Button>Nihil.</Button>
+      <Button>Temporibus!</Button>
+      <Button>Provident?</Button>
+      <Button>Fuga.</Button>
+      <Button>Earum.</Button>
     </>
   );
 }
