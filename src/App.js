1862d288e49847d98ce205e618b7559fc3b5407d
hoonil1 <phi0588@naver.com>
2023-11-02 15:36:34 +0900

    Image에 대해...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import { Box, Flex, FormControl, FormLabel, Switch } from "@chakra-ui/react";
+import { Box, Image, Stack } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <div>
-      <Box w={"480px"}>
-        <FormControl display={"flex"} alignItems={"center"}>
-          <FormLabel mb={0}>알림을 받으시겠습니까?</FormLabel>
-          <Switch size={"lg"} colorScheme={"red"} />
-        </FormControl>
+      <Stack direction="row">
+        <Image
+          boxSize="100px"
+          objectFit="cover"
+          src="https://bit.ly/dan-abramov"
+          alt="Dan Abramov"
+        />
+        <Image
+          boxSize="150px"
+          objectFit="cover"
+          src="https://bit.ly/dan-abramov"
+          alt="Dan Abramov"
+        />
+        <Image
+          boxSize="200px"
+          src="https://bit.ly/dan-abramov"
+          alt="Dan Abramov"
+        />
+      </Stack>
+
+      <Box>
+        <Image src="https://bit.ly/dan-abramov"></Image>
       </Box>
+      <Box>
+        <Image boxSize={"100px"} src="https://bit.ly/dan-abramov" />
+      </Box>
+      <Box>
+        <Image
+          borderRadius={"full"}
+          boxSize={"150px"}
+          src="https://bit.ly/dan-abramov"
+        />
+      </Box>
+      <Box>
+        <Image src="abcde.jpg" fallbackSrc="https://via.placeholder.com/150" />
+      </Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
     </div>
   );
 }
