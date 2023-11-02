4ae3684f25f6ee9160a9751f0b12590d245d37a8
hoonil1 <phi0588@naver.com>
2023-11-02 10:23:34 +0900

    필요없는 코드 삭제함

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import { Box } from "@chakra-ui/react";
-
 function App() {
-  return (
-    <>
-      <Box color={"white"} bgColor={"#9b9b9b"}>
-        Lorem ipsum dolor.
-      </Box>
-      <Box bgColor={"red"}>Lorem ipsum dolor.</Box>
-      <Box bgColor={"orange"}>Lorem ipsum dolor.</Box>
-      <Box bgColor={"yellow"}>Lorem ipsum dolor.</Box>
-      <Box bgColor={"green"}>Lorem ipsum dolor.</Box>
-      <Box bgColor={"blue"}>Lorem ipsum dolor.</Box>
-      <Box bgColor={"navy"}>Lorem ipsum dolor.</Box>
-      <Box bgColor={"purple"}>Lorem ipsum dolor.</Box>
-      <Box bgColor={"green.100"}>Lorem ipsum dolor.</Box>
-      <Box bgColor={"yellow.700"} color={"gray.200"}>
-        Lorem ipsum dolor.
-      </Box>
-      <Box bg={"facebook"}>Lorem ipsum dolor.</Box>
-      <Box>Lorem ipsum dolor.</Box>
-      <Box>Lorem ipsum dolor.</Box>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
