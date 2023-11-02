ba5b9a64cbe6f0c65530a0343f9541d2f6d0a5e8
hoonil1 <phi0588@naver.com>
2023-11-02 11:32:32 +0900

    Simple Grid 사용, 반응형 웹 적용

--- src/App.js
+++ src/App.js
@@ -1,22 +1,107 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
 
 function App() {
   return (
+    // Simple Grid 사이즈표
+    // base: "0em", // 0px
+    //   sm: "30em", // ~480px. em is a relative unit and is dependant on the font size.
+    //   md: "48em", // ~768px
+    //   lg: "62em", // ~992px
+    //   xl: "80em", // ~1280px
+    //   "2xl": "96em", // ~1536px
     <>
-      <Flex
-        w={"100%"}
-        h={"100px"}
-        bg={"#9b9b9b"}
-        gap={5}
-        align={"center"}
-        justify={"space-around"}
-      >
-        <Box bg={"red"}>Lorem.</Box>
-        <Box bg={"yellow"}>Culpa!</Box>
-        <Box bg={"blue"}>Aliquam.</Box>
-        <Box bg={"green"}>Provident.</Box>
-        <Box bg={"orange"}>Officia.</Box>
-      </Flex>
+      <SimpleGrid columns={{ base: 2, md: 3 }} spacing={"40px"}>
+        <Box bg="blue" color="white">
+          Lorem.
+        </Box>
+        <Box bg="blue" color="white">
+          Odit?
+        </Box>
+        <Box bg="blue" color="white">
+          Temporibus!
+        </Box>
+        <Box bg="blue" color="white">
+          Delectus!
+        </Box>
+        <Box bg="blue" color="white">
+          Nisi?
+        </Box>
+        <Box bg="blue" color="white">
+          Unde!
+        </Box>
+        <Box bg="blue" color="white">
+          Optio.
+        </Box>
+        <Box bg="blue" color="white">
+          Cupiditate!
+        </Box>
+        <Box bg="blue" color="white">
+          Mollitia.
+        </Box>
+        <Box bg="blue" color="white">
+          Nihil.
+        </Box>
+        <Box bg="blue" color="white">
+          Ab.
+        </Box>
+        <Box bg="blue" color="white">
+          Provident.
+        </Box>
+        <Box bg="blue" color="white">
+          Modi?
+        </Box>
+        <Box bg="blue" color="white">
+          Quidem?
+        </Box>
+        <Box bg="blue" color="white">
+          Eaque?
+        </Box>
+        <Box bg="blue" color="white">
+          Error.
+        </Box>
+        <Box bg="blue" color="white">
+          Dolore.
+        </Box>
+        <Box bg="blue" color="white">
+          Fugit!
+        </Box>
+        <Box bg="blue" color="white">
+          Odit.
+        </Box>
+        <Box bg="blue" color="white">
+          Et.
+        </Box>
+        <Box bg="blue" color="white">
+          Fugiat!
+        </Box>
+        <Box bg="blue" color="white">
+          Sunt!
+        </Box>
+        <Box bg="blue" color="white">
+          Quis!
+        </Box>
+        <Box bg="blue" color="white">
+          Facilis.
+        </Box>
+        <Box bg="blue" color="white">
+          Quae.
+        </Box>
+        <Box bg="blue" color="white">
+          Laboriosam?
+        </Box>
+        <Box bg="blue" color="white">
+          Ullam.
+        </Box>
+        <Box bg="blue" color="white">
+          Vitae.
+        </Box>
+        <Box bg="blue" color="white">
+          Porro!
+        </Box>
+        <Box bg="blue" color="white">
+          Quod.
+        </Box>
+      </SimpleGrid>
     </>
   );
 }
