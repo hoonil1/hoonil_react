dda13d780c329783ff967963d7a09ea0fa7a0e15
hoonil1 <phi0588@naver.com>
2023-11-02 15:29:21 +0900

    Switch에 대해...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Box, FormControl, Textarea, FormLabel } from "@chakra-ui/react";
+import { Box, Flex, FormControl, FormLabel, Switch } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <div>
       <Box w={"480px"}>
-        <FormControl>
-          <FormLabel>본문</FormLabel>
-          <Textarea placeholder={"본문을 작성하세요"} h={"500px"}></Textarea>
+        <FormControl display={"flex"} alignItems={"center"}>
+          <FormLabel mb={0}>알림을 받으시겠습니까?</FormLabel>
+          <Switch size={"lg"} colorScheme={"red"} />
         </FormControl>
       </Box>
     </div>
