39348707fd2fbb3e2dcaff14aebf240dc583e410
hoonil1 <phi0588@naver.com>
2023-11-02 14:54:07 +0900

    Form Control 에 대해서... RadioGroup, Radio, Checkbox 등등...

--- src/App.js
+++ src/App.js
@@ -1,46 +1,56 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
-import { PhoneIcon } from "@chakra-ui/icons";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Center,
+  Box,
+  RadioGroup,
+  Radio,
+  Flex,
+  CheckboxGroup,
+  Checkbox,
+} from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <Button colorScheme="blue">Lorem.</Button>
-      <Button colorScheme="red">Officia?</Button>
-      <Button colorScheme="yellow">Aliquam!</Button>
-      <Button colorScheme="green">Libero!</Button>
-      <Button colorScheme="orange">Quis.</Button>
-      <Button colorScheme="gray">Perspiciatis.</Button>
-      <Button size={"xs"}>Pariatur!</Button>
-      <Button size={"sm"}>Voluptatum!</Button>
-      <Button size={"md"}>Cupiditate?</Button>
-      <Button size={"lg"}>Sit.</Button>
-      <Button colorScheme="red" variant={"outline"}>
-        Inventore.
-      </Button>
-      <Button colorScheme="facebook" variant={"outline"}>
-        Officiis.
-      </Button>
-      <Button variant={"ghost"}>Blanditiis.</Button>
-      <Button leftIcon={<PhoneIcon />}>Beatae.</Button>
-      <Button isLoading loadingText={"Connecting"}>
-        Similique!
-      </Button>
-      <Button>Accusantium.</Button>
-      <Button>Repudiandae.</Button>
-      <Button>Obcaecati?</Button>
-      <Button>Neque?</Button>
-      <Button>Enim.</Button>
-      <Button>Veritatis?</Button>
-      <Button>Id.</Button>
-      <Button>Eligendi.</Button>
-      <Button>Sequi?</Button>
-      <Button>Veritatis?</Button>
-      <Button>Nihil.</Button>
-      <Button>Temporibus!</Button>
-      <Button>Provident?</Button>
-      <Button>Fuga.</Button>
-      <Button>Earum.</Button>
-    </>
+    <div>
+      <Center>
+        <Box w={"400px"}>
+          <FormControl>
+            <FormLabel>Email address</FormLabel>
+            <Input type="email" />
+            <FormHelperText>We'll never share your email.</FormHelperText>
+          </FormControl>
+        </Box>
+      </Center>
+      <Center mt={5}>
+        <FormControl>
+          <FormLabel>Choose Your Favorite Player</FormLabel>
+          <RadioGroup>
+            <Flex gap={2}>
+              <Radio value="value1">C.Ronaldo</Radio>
+              <Radio value="value2">L.Messi</Radio>
+              <Radio value="value3">Neymar JR</Radio>
+              <Radio value="value4">Mbappe</Radio>
+            </Flex>
+          </RadioGroup>
+          <FormHelperText>Choose Just One!</FormHelperText>
+        </FormControl>
+      </Center>
+      <Center mt={5}>
+        <FormControl>
+          <CheckboxGroup>
+            <Flex gap={2}>
+              <Checkbox>SON</Checkbox>
+              <Checkbox>LEE</Checkbox>
+              <Checkbox>KIM</Checkbox>
+            </Flex>
+          </CheckboxGroup>
+        </FormControl>
+      </Center>
+    </div>
   );
 }
 
