797e29129985be9983f13db6ea9a5a0754fd312c
hoonil1 <phi0588@naver.com>
2023-11-02 12:30:18 +0900

    Table 작성, 및 사용 Table,Tbody,Thead,Th,Tr,Td

--- src/App.js
+++ src/App.js
@@ -8,252 +8,83 @@ import {
   Flex,
   SimpleGrid,
   Button,
+  Table,
+  Th,
+  Tbody,
+  Thead,
+  Tr,
+  Td,
 } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <SimpleGrid gap={3} columns={{ base: 2, xl: 5 }}>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Lorem ipsum.</Heading>
-          </CardHeader>
-          <CardBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
-            illo, inventore mollitia nostrum perspiciatis porro.
-          </CardBody>
-          <CardFooter>
-            <Button>Lorem.</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Architecto, distinctio.</Heading>
-          </CardHeader>
-          <CardBody>
-            Adipisci commodi, distinctio, dolore laudantium modi nisi numquam
-            obcaecati porro quasi sit soluta ut vel.
-          </CardBody>
-          <CardFooter>
-            <Button>Ab.</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Illo, laborum?</Heading>
-          </CardHeader>
-          <CardBody>
-            At consectetur dicta eos labore, omnis quas voluptatem. Ad
-            cupiditate dolor excepturi fugit maxime vero.
-          </CardBody>
-          <CardFooter>
-            <Button>Placeat?</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Asperiores, error.</Heading>
-          </CardHeader>
-          <CardBody>
-            Autem consectetur cum cumque expedita explicabo impedit laboriosam
-            modi, perferendis perspiciatis possimus sapiente sequi totam.
-          </CardBody>
-          <CardFooter>
-            <Button>Est!</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Laboriosam, totam.</Heading>
-          </CardHeader>
-          <CardBody>
-            Architecto cum ducimus odit quia voluptate? Animi, eligendi esse
-            fuga illum modi quos voluptatem voluptatum.
-          </CardBody>
-          <CardFooter>
-            <Button>Assumenda!</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Doloribus, perspiciatis!</Heading>
-          </CardHeader>
-          <CardBody>
-            Aperiam architecto atque deleniti eligendi incidunt labore laborum,
-            modi, nobis officia optio quo repellat, tempore.
-          </CardBody>
-          <CardFooter>
-            <Button>Deserunt?</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Ab, impedit?</Heading>
-          </CardHeader>
-          <CardBody>
-            Animi ea ipsa voluptate? Aliquam commodi iste libero nobis optio
-            quisquam quod, reiciendis saepe ullam.
-          </CardBody>
-          <CardFooter>
-            <Button>Molestiae?</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Amet, delectus.</Heading>
-          </CardHeader>
-          <CardBody>
-            Aliquam doloremque esse fugit illo impedit rerum temporibus. Amet
-            atque distinctio iure, laudantium nostrum quisquam?
-          </CardBody>
-          <CardFooter>
-            <Button>Deleniti.</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Accusantium, modi?</Heading>
-          </CardHeader>
-          <CardBody>
-            Ad amet delectus eos exercitationem expedita illum minus natus
-            placeat provident quidem, suscipit velit, voluptatibus.
-          </CardBody>
-          <CardFooter>
-            <Button>Autem.</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Delectus, dolore.</Heading>
-          </CardHeader>
-          <CardBody>
-            Animi, corporis ea ex id in magnam maxime minus molestias nemo
-            obcaecati, praesentium quasi sunt.
-          </CardBody>
-          <CardFooter>
-            <Button>Repellendus.</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Alias, eos.</Heading>
-          </CardHeader>
-          <CardBody>
-            Est hic impedit quae. Beatae debitis dignissimos, ipsa iusto
-            molestiae molestias necessitatibus nesciunt officia sed.
-          </CardBody>
-          <CardFooter>
-            <Button>Doloremque.</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Ea, possimus.</Heading>
-          </CardHeader>
-          <CardBody>
-            Deserunt eos eveniet ex ipsa labore magnam molestiae, possimus quod
-            sed tempore temporibus veniam veritatis?
-          </CardBody>
-          <CardFooter>
-            <Button>Libero.</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Minima, modi?</Heading>
-          </CardHeader>
-          <CardBody>
-            Deserunt dicta dolore molestiae odit, perspiciatis quam, qui quo
-            repellat reprehenderit unde ut, veniam vitae.
-          </CardBody>
-          <CardFooter>
-            <Button>Quasi.</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Cupiditate, maiores!</Heading>
-          </CardHeader>
-          <CardBody>
-            Accusantium alias assumenda deleniti ea eaque est exercitationem
-            explicabo, fuga illum iusto modi obcaecati tempora!
-          </CardBody>
-          <CardFooter>
-            <Button>Culpa.</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Facere, reiciendis?</Heading>
-          </CardHeader>
-          <CardBody>
-            Accusamus blanditiis exercitationem facere, fuga fugit labore maxime
-            nostrum perferendis repellendus voluptate! Natus repudiandae, vel?
-          </CardBody>
-          <CardFooter>
-            <Button>Cupiditate?</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Iste, reprehenderit!</Heading>
-          </CardHeader>
-          <CardBody>
-            Iure, modi, vitae. Ab aut dicta dolore doloremque eligendi, libero
-            maiores molestiae nesciunt! Fugit, numquam.
-          </CardBody>
-          <CardFooter>
-            <Button>Quam.</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Ex, numquam.</Heading>
-          </CardHeader>
-          <CardBody>
-            Dignissimos et hic quas repudiandae ullam? Earum et expedita fuga
-            iusto repellendus repudiandae rerum tempore.
-          </CardBody>
-          <CardFooter>
-            <Button>Facilis!</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Quam, ullam?</Heading>
-          </CardHeader>
-          <CardBody>
-            Eligendi ex facere fugit in inventore, iste magnam neque perferendis
-            quam quos sequi vero voluptatem.
-          </CardBody>
-          <CardFooter>
-            <Button>Iste.</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Commodi, iusto!</Heading>
-          </CardHeader>
-          <CardBody>
-            Animi architecto consequuntur distinctio ducimus est ipsam libero
-            nam, quaerat quo quos tempore totam velit!
-          </CardBody>
-          <CardFooter>
-            <Button>Nam!</Button>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Animi, necessitatibus!</Heading>
-          </CardHeader>
-          <CardBody>
-            Amet dolorem facere molestias quia rerum. Accusantium asperiores
-            dicta laudantium maiores officiis provident quasi sint?
-          </CardBody>
-          <CardFooter>
-            <Button>Dolor.</Button>
-          </CardFooter>
-        </Card>
+      <SimpleGrid columns={1} gap={5}>
+        <Box>
+          {/* Table>Thead>Tr>Th*4>lorem1^^^Tbody>Tr*5>Td*4>lorem1 */}
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>Lorem.</Th>
+                <Th>Inventore!</Th>
+                <Th>Repellat.</Th>
+                <Th>Sapiente!</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Lorem.</Td>
+                <Td>Eius.</Td>
+                <Td>Ex?</Td>
+                <Td>Neque.</Td>
+              </Tr>
+              <Tr>
+                <Td>Lorem.</Td>
+                <Td>Voluptas.</Td>
+                <Td>Maxime?</Td>
+                <Td>Obcaecati!</Td>
+              </Tr>
+              <Tr>
+                <Td>Lorem.</Td>
+                <Td>Labore.</Td>
+                <Td>Veniam.</Td>
+                <Td>Consequuntur.</Td>
+              </Tr>
+              <Tr>
+                <Td>Lorem.</Td>
+                <Td>Quas.</Td>
+                <Td>Iure?</Td>
+                <Td>Sed.</Td>
+              </Tr>
+              <Tr>
+                <Td>Lorem.</Td>
+                <Td>Officia?</Td>
+                <Td>Excepturi.</Td>
+                <Td>Minus.</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+        <Box>
+          <Table variant="simple" size={"sm"}>
+            <Thead>
+              <Tr>
+                <Th>Lorem.</Th>
+                <Th>Hic!</Th>
+                <Th>Odio?</Th>
+                <Th>Corporis?</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Lorem.</Td>
+                <Td>Nam!</Td>
+                <Td>Repellat?</Td>
+                <Td>Facere.</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
       </SimpleGrid>
     </>
   );
