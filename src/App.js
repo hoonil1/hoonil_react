9b486455faf24d99ed9e70b97b5be7185c2bb7a9
hoonil1 <phi0588@naver.com>
2023-11-02 12:37:31 +0900

    Center, Square, Circle 사용 및 Icon 삽입

--- src/App.js
+++ src/App.js
@@ -14,78 +14,29 @@ import {
   Thead,
   Tr,
   Td,
+  Center,
+  Circle,
+  Square,
 } from "@chakra-ui/react";
+import { ChatIcon, PhoneIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
     <>
-      <SimpleGrid columns={1} gap={5}>
-        <Box>
-          {/* Table>Thead>Tr>Th*4>lorem1^^^Tbody>Tr*5>Td*4>lorem1 */}
-          <Table>
-            <Thead>
-              <Tr>
-                <Th>Lorem.</Th>
-                <Th>Inventore!</Th>
-                <Th>Repellat.</Th>
-                <Th>Sapiente!</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Eius.</Td>
-                <Td>Ex?</Td>
-                <Td>Neque.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Voluptas.</Td>
-                <Td>Maxime?</Td>
-                <Td>Obcaecati!</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Labore.</Td>
-                <Td>Veniam.</Td>
-                <Td>Consequuntur.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Quas.</Td>
-                <Td>Iure?</Td>
-                <Td>Sed.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Officia?</Td>
-                <Td>Excepturi.</Td>
-                <Td>Minus.</Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </Box>
-        <Box>
-          <Table variant="simple" size={"sm"}>
-            <Thead>
-              <Tr>
-                <Th>Lorem.</Th>
-                <Th>Hic!</Th>
-                <Th>Odio?</Th>
-                <Th>Corporis?</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Nam!</Td>
-                <Td>Repellat?</Td>
-                <Td>Facere.</Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </Box>
-      </SimpleGrid>
+      <Center bg={"Red"} h={"200px"} gap={"50px"}>
+        <Square
+          bg="blue"
+          w={"100px"}
+          h={"100px"}
+          color={"white"}
+          fontSize={"2rem"}
+        >
+          <ChatIcon />
+        </Square>
+        <Circle bg={"blue"} w={"100px"} h={"100px"} color={"white"}>
+          <PhoneIcon />
+        </Circle>
+      </Center>
     </>
   );
 }
