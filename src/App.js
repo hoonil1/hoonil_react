83c52a00ef8a36259f39c17e850a12fa1c1aa2b9
hoonil1 <phi0588@naver.com>
2023-11-03 14:48:55 +0900

    state 가 바뀌면 컴포넌트가 다시 그려진다 (re render)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from "react";
-import { Input } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Button, Input } from "@chakra-ui/react";
 
 function App(props) {
-  let text = "initial text";
-  function handleChange(e) {
-    // console.log("input value changed");
-    console.log(e.target.value);
+  // 상태가 변경되면 컴포넌트를 다시 그린다 ( = re render)
+  // state : react가 관리하는 컴포넌트의 값
+
+  // state 를 얻는 방법
+  // useState();
+  const [, set] = useState(); //  배열을 리턴함
+  // index 0 :  현재 상태
+  // index 1 : state를 변경할수 있는 메소드
+  const [state, func] = useState("");
+
+  let text = "초기 메세지";
+  function handleInputChange(e) {
     text = e.target.value;
+    console.log("text" + text);
+  }
+
+  function handleButtonClick(e) {
+    func("바뀐 상태(값)");
   }
   return (
     <div>
+      <Input onChange={handleInputChange} />
       <p>{text}</p>
-      <Input onChange={handleChange}></Input>
-      <Input onChange={handleChange}></Input>
+      <Button onClick={handleButtonClick}>상태바꾸기</Button>
+      <Button onClick={() => func("")}>빈 문자열로 바꾸기</Button>
+      <p>{state}</p>
     </div>
   );
 }
