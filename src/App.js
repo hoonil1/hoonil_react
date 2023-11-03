9753be18a235eafdc38940e7f7f09f5b35c9920d
hoonil1 <phi0588@naver.com>
2023-11-03 09:34:20 +0900

    children, link만들기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Button } from "@chakra-ui/react";
+
+function MyLink({ to, children }) {
+  return (
+    <div>
+      <Button as="a" href={to} colorScheme="green">
+        NAVER
+      </Button>
+      <Button as={"a"} href={children} colorScheme="blue">
+        DAUM
+      </Button>
+    </div>
+  );
+}
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <MyLink to={"https://www.naver.com"}>네이버로 </MyLink>
+      <MyLink to={"https://www.daum.net"}>다음으로 </MyLink>
+
+      {/*  
+  <a href="http://www.naver.com">네이버로</a>
+  <a href="http://www.daum.net">다음으로</a>
+  */}
+    </>
+  );
 }
 
 export default App;
