2638ed2790f39aa3d95e4e1fd33d8748ff066683
hoonil1 <phi0588@naver.com>
2023-11-03 14:57:21 +0900

    state 가 바뀌면 컴포넌트가 다시 그려진다 (re render)
    상태를 변경할때 set 메소드 사용해야한다

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React, { useState } from "react";
-import { Button, Input } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 
 function App(props) {
-  // 상태가 변경되면 컴포넌트를 다시 그린다 ( = re render)
-  // state : react가 관리하는 컴포넌트의 값
-
-  // state 를 얻는 방법
-  // useState();
-  const [, set] = useState(); //  배열을 리턴함
-  // index 0 :  현재 상태
-  // index 1 : state를 변경할수 있는 메소드
-  const [state, func] = useState("");
-
-  let text = "초기 메세지";
-  function handleInputChange(e) {
-    text = e.target.value;
-    console.log("text" + text);
-  }
-
-  function handleButtonClick(e) {
-    func("바뀐 상태(값)");
+  let [number, setNumber] = useState(0);
+  function handleClick() {
+    // number++;
+    setNumber(number + 1);
   }
   return (
     <div>
-      <Input onChange={handleInputChange} />
-      <p>{text}</p>
-      <Button onClick={handleButtonClick}>상태바꾸기</Button>
-      <Button onClick={() => func("")}>빈 문자열로 바꾸기</Button>
-      <p>{state}</p>
+      <Button onClick={handleClick}>바꾸기</Button>
+      <Text>{number}</Text>
     </div>
   );
 }
