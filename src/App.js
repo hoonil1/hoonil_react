df93bedb1856c32e4b3336222a8ffec265f3b5f0
hoonil1 <phi0588@naver.com>
2023-11-03 15:48:00 +0900

    state 사용 해서 문제 해결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Input } from "@chakra-ui/react";
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  function handleInput1(e) {
+    setNumber1(Number(e.target.value));
+  }
+  function handleInput2(e) {
+    setNumber2(Number(e.target.value));
+  }
+  let result = number1 + number2;
+
+  return (
+    <>
+      <Input type={"number"} onChange={handleInput1} /> +
+      <Input type={"number"} onChange={handleInput2} /> ={result}
+    </>
+  );
 }
 
 export default App;
