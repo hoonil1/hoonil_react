cb4cc670054d801597f79a7995ae89e0233b0cf4
hoonil1 <phi0588@naver.com>
2023-11-03 09:44:35 +0900

    children prop 연습완료

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import { Button } from "@chakra-ui/react";
+import { Card, CardBody } from "@chakra-ui/react";
 
-function MyLink({ to, children }) {
-  return (
-    <div>
-      <Button as="a" href={to} colorScheme="green">
-        NAVER
-      </Button>
-      <Button as={"a"} href={children} colorScheme="blue">
-        DAUM
-      </Button>
-    </div>
-  );
-}
-
-function App() {
+function MyBox({ color, bg, children }) {
   return (
     <>
-      <MyLink to={"https://www.naver.com"}>네이버로 </MyLink>
-      <MyLink to={"https://www.daum.net"}>다음으로 </MyLink>
-
-      {/*  
-  <a href="http://www.naver.com">네이버로</a>
-  <a href="http://www.daum.net">다음으로</a>
-  */}
+      <Card color={color} bg={bg}>
+        <CardBody>{children}</CardBody>
+      </Card>
     </>
+  );
+}
+function App(props) {
+  return (
+    <div>
+      <MyBox color={"blue"} bg={"yellow"} children={"hello"} />
+    </div>
   );
 }
 
