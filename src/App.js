6546467a5b73be9d7bd94f7d2bd8f45bbeba8783
hoonil1 <phi0588@naver.com>
2023-11-03 11:40:26 +0900

    event handler 메소드에 event 객체가 전달된다.

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import React from "react";
 import { Button } from "@chakra-ui/react";
 
 function App(props) {
-  function handleClick() {
-    console.log("clicked");
-  }
-  function handleClick2() {
-    console.log("clicked2");
-  }
-  function handleClick3(number) {
-    console.log(number + "번째 버튼 클릭됨");
+  // 함수의 이름이 event handle 메소드일때
+  // 브라우저는 event 객체를 매개값으로 넣어준다 (브라우저가)
+  function handleClick(event) {
+    // console.log(event.target.className);
+    // console.log(event.target.type);
+    console.log(event.target);
   }
   return (
     <div>
       <Button onClick={handleClick}>Button1</Button>
-      <Button onClick={handleClick2}>Button2</Button>
-      <Button onClick={() => handleClick3(1)}>Button3</Button>
-      <Button onClick={() => handleClick3(5)}>Button4</Button>
+      <Button onClick={handleClick}>Button2</Button>
     </div>
   );
 }
