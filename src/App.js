d0b0e189eef097acd3b6f4ec6176c9ab302ba82b
hoonil1 <phi0588@naver.com>
2023-11-03 12:33:27 +0900

    preventDefault로 원래 하려던 작업을 취소하고 새 작업을 시작하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 function App() {
-  return <></>;
+  function handleClick(e) {
+    e.preventDefault();
+    window.location.href = "https://www.naver.com";
+  }
+  // a태그 클릭시 naver로 이동하도록 코드작성
+  return (
+    <>
+      <a href="https://www.daum.net" onClick={handleClick}>
+        다음으로 이동
+      </a>
+    </>
+  );
 }
 
 export default App;
