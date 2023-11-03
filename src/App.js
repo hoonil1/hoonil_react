e15a7c94a2d9a27a700342e757c8785356d9e37d
hoonil1 <phi0588@naver.com>
2023-11-03 10:21:27 +0900

    함수를 props로 전달

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import { Card, CardBody } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
-function MyBox({ color, bg, children }) {
+function MyComp({ to, someProp }) {
+  someProp();
   return (
     <>
-      <Card color={color} bg={bg}>
-        <CardBody>{children}</CardBody>
-      </Card>
+      <div>
+        <a href={to[0]}>link~</a>
+      </div>
+      <div>
+        <Button onClick={someProp}>버튼</Button>
+      </div>
     </>
   );
 }
+
 function App(props) {
+  function func1() {
+    console.log("func1 실행됨");
+  }
+
   return (
     <div>
-      <MyBox color={"blue"} bg={"yellow"} children={"hello"} />
+      <MyComp to={["https://www.naver.com"]}></MyComp>
+      <MyComp to={["https://www.naver.com"]} someProp={func1}></MyComp>
+      {/*  배열도 삽입 가능*/}
     </div>
   );
 }
