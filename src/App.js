0f4dedd48026df52d9433e7a20c8179bcb38ab2b
hoonil1 <phi0588@naver.com>
2023-11-03 09:10:16 +0900

    페이지 메뉴 만들어보기 실습

--- src/App.js
+++ src/App.js
@@ -1,52 +1,64 @@
-import { Box, Image, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Select,
+  Stack,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Input,
+} from "@chakra-ui/react";
+import { TriangleDownIcon } from "@chakra-ui/icons";
 
 function App(props) {
   return (
-    <div>
-      <Stack direction="row">
-        <Image
-          boxSize="100px"
-          objectFit="cover"
-          src="https://bit.ly/dan-abramov"
-          alt="Dan Abramov"
-        />
-        <Image
-          boxSize="150px"
-          objectFit="cover"
-          src="https://bit.ly/dan-abramov"
-          alt="Dan Abramov"
-        />
-        <Image
-          boxSize="200px"
-          src="https://bit.ly/dan-abramov"
-          alt="Dan Abramov"
-        />
-      </Stack>
-
-      <Box>
-        <Image src="https://bit.ly/dan-abramov"></Image>
-      </Box>
-      <Box>
-        <Image boxSize={"100px"} src="https://bit.ly/dan-abramov" />
-      </Box>
-      <Box>
-        <Image
-          borderRadius={"full"}
-          boxSize={"150px"}
-          src="https://bit.ly/dan-abramov"
-        />
-      </Box>
-      <Box>
-        <Image src="abcde.jpg" fallbackSrc="https://via.placeholder.com/150" />
-      </Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-    </div>
+    <Box
+      w={"700px"}
+      h={"80px"}
+      bg={"#9b9b9b"}
+      px={{ base: "0", md: "70px", lg: "120px", xl: "180px" }}
+    >
+      <Flex align={"center"}>
+        <Stack spacing={1} direction={"row"} mt={4}>
+          <Button size={"md"} colorScheme="#9b9b9b" fontStyle={"bold"}>
+            Navbar
+          </Button>
+          <Button size={"sm"} colorScheme="#9b9b9b">
+            Home
+          </Button>
+          <Button size={"sm"} colorScheme="#9b9b9b">
+            Link
+          </Button>
+          <Menu>
+            <MenuButton
+              as={Button}
+              rightIcon={<TriangleDownIcon />}
+              bg={"#9b9b9b"}
+              color={"white"}
+              size={"sm"}
+            >
+              Dropdown
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Action</MenuItem>
+              <MenuItem>Another Action</MenuItem>
+              <hr />
+              <MenuItem>Something Else Here</MenuItem>
+            </MenuList>
+          </Menu>
+          <Button size={"sm"} colorScheme="#9b9b9b" isDisabled>
+            Disabled
+          </Button>
+          <Input w={"170px"} bg={"white"} placeholder="Search" />
+          <Button colorScheme="teal" variant={"outline"}>
+            Search
+          </Button>
+        </Stack>
+      </Flex>
+    </Box>
   );
 }
 
