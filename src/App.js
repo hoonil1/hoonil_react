2e546fa3c4bfec36147746d9319d9dcd554564be
hoonil1 <phi0588@naver.com>
2023-11-06 12:23:40 +0900

    배열을 jsx에서 사용하기, 리스트아이템에 매핑되는 컴포넌트는 key prop 을 꼭 가져야한다.
    key prop은 유일한 값이어야 하며, 주로 database의 primary key 가 사용될 것이다

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import React from "react";
 import { Box, List, ListItem } from "@chakra-ui/react";
 
-function App(props) {
+function App() {
   const arr = ["pizza", "burger"];
   const arr2 = ["son", "lee", "kim"];
   const arr3 = ["latte", "americano", "esspresso"];
+  const arr4 = [
+    {id:0,"coffee"},
+    {id:1,"cheese"},
+  ];
   return (
     <div>
-      <Box>
+      <Box mb={3}>
         <List>
-          {arr.map((e) => (
-            <ListItem>{e}</ListItem>
+          {arr.map((e, index) => (
+            <ListItem key={index}>{e}</ListItem>
+          ))}
+        </List>
+      </Box>
+      <Box mb={3}>
+        <List>
+          {arr2.map((x, i) => (
+            <ListItem key={i}>{x}</ListItem>
           ))}
         </List>
       </Box>
       <Box>
         <List>
-          {arr2.map((x) => (
-            <ListItem>{x}</ListItem>
-          ))}
-        </List>
-      </Box>
-      <Box>
-        <List>
-          {arr3.map((y) => (
-            <ListItem>{y}</ListItem>
+          {arr3.map((y, z) => (
+            <ListItem key={z}>{y}</ListItem>
           ))}
         </List>
       </Box>
