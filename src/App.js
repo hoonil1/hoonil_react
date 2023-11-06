d199225e6442ea33afb0a712648e3cebc5f4dffc
hoonil1 <phi0588@naver.com>
2023-11-06 14:16:54 +0900

    배열을 상태로 쓸 경우, 상태 변경시 새로운 배열로 복사해서 사용해야한다. 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
-function App() {
-  return <></>;
+import React, { useState } from "react";
+import { Box, Button, ListItem, OrderedList } from "@chakra-ui/react";
+
+function App(props) {
+  const [items, setItems] = useState([]);
+
+  function handleButtonClick(item) {
+    console.log(item);
+    // items 의 상태를 item 을 추가하도록 변경
+    // const nextItems = [...items];
+    // nextItems.push(item);
+    // const nextItems = [...items, item];
+    // setItems(nextItems);
+    setItems([...items, item]);
+    // 다 같은 코드
+  }
+
+  return (
+    <div>
+      <Button onClick={() => handleButtonClick("coffee")}>1</Button>
+      <Button onClick={() => handleButtonClick("cake")}>2</Button>
+      <Button onClick={() => handleButtonClick("icecream")}>3</Button>
+      <Button onClick={() => handleButtonClick("bread")}>4</Button>
+      <Button onClick={() => handleButtonClick("chicken")}>5</Button>
+      <Box>
+        <OrderedList>
+          {items.map((item, index) => (
+            <ListItem key={index}>{item}</ListItem>
+          ))}
+        </OrderedList>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
