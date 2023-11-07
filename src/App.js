6e3a4a87e1c4d26b9a3f762fed4e0b1a2557878e
hoonil1 <phi0588@naver.com>
2023-11-07 10:18:46 +0900

    다른 컴포넌트의 상태 바꾸기 : 공통 부모에게 상태를 두고 사용하여야 한다 22 연습

--- src/App.js
+++ src/App.js
@@ -1,71 +1,32 @@
 import React, { useState } from "react";
-import { Box, Input, Text } from "@chakra-ui/react";
-import { useImmer } from "use-immer";
+import * as PropTypes from "prop-types";
+import { Box, Button, Text } from "@chakra-ui/react";
+
+function MyComp1({ onClick }) {
+  function handleButtonClick() {
+    onClick("안녕하세요");
+  }
+  return (
+    <Box>
+      <Button onClick={handleButtonClick}>텍스트 바꾸기!</Button>
+    </Box>
+  );
+}
+
+function MyComp2({ message }) {
+  return (
+    <Box>
+      <Text>{message}</Text>
+    </Box>
+  );
+}
 
 function App(props) {
-  const [person1, setPerson1] = useState({
-    name: "son",
-    city: "seoul",
-    email: "son@gmail",
-  });
-
-  const [person2, updatePerson2] = useImmer({
-    name: "kim",
-    city: "daegu",
-    email: "kim@gmail.com",
-  });
-
-  function handleNameChange1(e) {
-    const copyPerson1 = { ...person1 };
-    copyPerson1.name = e.target.value;
-    setPerson1(copyPerson1);
-  }
-
-  function handleCityChange1(e) {
-    setPerson1({ ...person1, city: e.target.value });
-  }
-
-  function handleEmailChange1(e) {
-    setPerson1({ ...person1, email: e.target.value });
-  }
-
-  function handleNameChange2(e) {
-    updatePerson2((draft) => {
-      draft.name = e.target.value;
-    });
-  }
-
-  function handleCityChange2(e) {
-    updatePerson2((draft) => {
-      draft.city = e.target.value;
-    });
-  }
-
-  function handleEmailChange2(e) {
-    updatePerson2((draft) => {
-      draft.email = e.target.value;
-    });
-  }
-
+  const [message, setMessage] = useState("hello");
   return (
     <div>
-      <Box>
-        <Input value={person1.name} onChange={handleNameChange1} />
-        <Text>이름 : {person1.name}</Text>
-        <Input value={person1.city} onChange={handleCityChange1} />
-        <Text>도시 : {person1.city}</Text>
-        <Input value={person1.email} onChange={handleEmailChange1} />
-        <Text>이메일 : {person1.email}</Text>
-      </Box>
-      <hr />
-      <Box>
-        <Input value={person2.name} onChange={handleNameChange2} />
-        <Text>NAME : {person2.name}</Text>
-        <Input value={person2.city} onChange={handleCityChange2} />
-        <Text>CITY : {person2.city}</Text>
-        <Input value={person2.email} onChange={handleEmailChange2} />
-        <Text>EMAIL : {person2.email}</Text>
-      </Box>
+      <MyComp1 onClick={setMessage}>{/*  버튼이 클릭되면*/}</MyComp1>
+      <MyComp2 message={message}>{/*    텍스트가 바뀌게*/}</MyComp2>
     </div>
   );
 }
