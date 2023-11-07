b44a49327b6fc75a95ea41d89dd82bc14b43b128
hoonil1 <phi0588@naver.com>
2023-11-07 17:06:04 +0900

    ajax get 사용 연습 완료 예제 풀이 완료

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App(props) {
-  const [message, setMessage] = useState("");
+  const [customer, setCustomer] = useState("");
   useEffect(() => {
     axios
-      .get("/api/main1/sub2")
+      .get("/api/main1/sub3")
       .then((response) => response.data)
-      .then((data) => setMessage(data))
-      .catch((error) => console.log(error));
+      .then((data) => setCustomer(data))
+      .then((error) => console.log(error));
   }, []);
-  return <div>{message}</div>;
+  return (
+    <div>
+      <h1>{customer.id}</h1>
+      <h1>{customer.name}</h1>
+      <h1>{customer.contactName}</h1>
+      <h1>{customer.address}</h1>
+      <h1>{customer.city}</h1>
+      <h1>{customer.postalCode}</h1>
+      <h1>{customer.country}</h1>
+    </div>
+  );
 }
 
 export default App;
