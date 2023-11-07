fc659c982a16d37592a72e87715dc8640d4570e8
hoonil1 <phi0588@naver.com>
2023-11-07 14:38:57 +0900

    다른 파일에 있는 component가 context를 사용하기 위해서 context를 export 해야한다

---
+++ src/BComp.js
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+import { Text } from "@chakra-ui/react";
+import { MessageContext } from "./App";
+
+export function BComp() {
+  // 3. context 사용하기
+  const { message } = useContext(MessageContext);
+  return <Text>{message}</Text>;
+}
