3d204d34386fca5dce2c8490121b07d729c87177
hoonil1 <phi0588@naver.com>
2023-11-07 16:30:22 +0900

    ajax get 사용 연습 완료

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App(props) {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    axios
+      .get("/api/main1/sub2")
+      .then((response) => response.data)
+      .then((data) => setMessage(data))
+      .catch((error) => console.log(error));
+  }, []);
+  return <div>{message}</div>;
+}
+
+export default App;
