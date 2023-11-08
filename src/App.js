d39a1b6983a13ba42a54c37247135d9759d1d944
hoonil1 <phi0588@naver.com>
2023-11-08 10:25:02 +0900

    api/main1/sub4 고객아이디로 고객정보 조회, ResponseEntity 활용

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { Box, Select, Text } from "@chakra-ui/react";
+import { Box, Select, Spinner, Text } from "@chakra-ui/react";
 import axios from "axios";
 
 function App(props) {
   const [customerId, setCustomerId] = useState(0);
   const [customer, setCustomer] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get("/api/main1/sub4?id=" + customerId)
       .then((response) => response.data)
       .then((data) => setCustomer(data))
-      .catch((error) => console.log(error))
-      .finally(() => console.log("end"));
+      .catch(() => setCustomer(null))
+      .finally(() => setIsLoading(false));
   }, [customerId]);
   return (
     <div>
@@ -31,7 +33,16 @@ function App(props) {
         <option value="10">10</option>
       </Select>
       <Box>
-        <Text>고객 이름 : {customer.name}</Text>
+        {isLoading && <Spinner />}
+        {isLoading || (
+          <>
+            {customer === null ? (
+              <Text>조회한 고객이 없습니다</Text>
+            ) : (
+              <Text>고객 이름 : {customer.name}</Text>
+            )}
+          </>
+        )}
       </Box>
     </div>
   );
