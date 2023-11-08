fa6c9b06ff25217753a903bac980c6a88cca81d1
hoonil1 <phi0588@naver.com>
2023-11-08 16:52:34 +0900

    react router의 useParams로 dynamic param(pathvariable 과유사) 읽기

--- src/App.js
+++ src/App.js
@@ -6,15 +6,27 @@ import {
   Route,
   RouterProvider,
   useNavigate,
+  useParams,
   useSearchParams,
 } from "react-router-dom";
 import { Box, Button, Text } from "@chakra-ui/react";
 import axios from "axios";
 
+function BComp() {
+  // dynamic param 을 얻는 hook -> useParams()
+  const params = useParams();
+  console.log(params);
+  console.log(params.address);
+  return <Box>B Component {params.address}</Box>;
+}
+
 const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Home />}>
       <Route path="path1" element={<AComp />} />
+
+      {/* dynamic param : spring web mvc의 path variable 과 같다*/}
+      <Route path="path2/:address" element={<BComp />} />
     </Route>,
   ),
 );
@@ -26,6 +38,9 @@ function Home() {
         <Button onClick={() => navigate("/path1?id=1")}>1번 고객 보기</Button>
         <Button onClick={() => navigate("/path1?id=2")}>2번 고객 보기</Button>
         <Button onClick={() => navigate("/path1?id=3")}>3번 고객 보기</Button>
+
+        <Button onClick={() => navigate("/path2/seoul")}>seoul 보기</Button>
+        <Button onClick={() => navigate("/path2/busan")}>busan 보기</Button>
       </Box>
       <Box>
         <Outlet />
