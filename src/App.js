e6baf83a255ac19a7e029b4ce1a831f49da5fcd3
hoonil1 <phi0588@naver.com>
2023-11-08 14:54:27 +0900

    useEffect 실수123

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React, { useEffect, useState } from "react";
+import { Box, Button, Checkbox, Text } from "@chakra-ui/react";
+
+function MyComp({ color }) {
+  const [number, setNumber] = useState(0);
+  useEffect(() => {
+    console.log(color + " : initial render");
+  }, []);
+  console.log(color + ": re render");
+  return (
+    <Box borderWidth={"10px"} borderColor={color}>
+      <Button onClick={() => setNumber(number + 1)}>증가</Button>
+      <Text>{number}</Text>
+    </Box>
+  );
+}
 
 function App(props) {
+  // 부모가 렌더링될 때 자식도 렌더링 된다.
   const [number, setNumber] = useState(0);
-  // useEffect 자주 하는 실수
-  useEffect(() => {
-    console.log("코드 실행");
-    setNumber(number + 1); // trigger 하는 값을 변경하면 안된다.
-    // aws 서버에 올리는 코드를 실수로 잘못하면
-    // 몇천번 이상의 오류로 aws 무료 기준을 초과한다.
-  }, [number]);
-  return <div></div>;
+  const [secondToggle, setSecondToggle] = useState(true);
+  console.log("부모 re-rendering");
+  return (
+    <div>
+      <Box>
+        <Text>부모</Text>
+        <Button onClick={() => setNumber(number + 1)}>증가</Button>
+        <Text>{number}n</Text>
+        <Checkbox defalutChecked-{true}
+          onChange={(e)}
+          onChange={(e) => console.log(e.target.checked)}
+        ></Checkbox>{" "}
+        파란 박스 토글
+      </Box>
+      <Box mt={5}>
+        <Text>자식들</Text>
+        <MyComp color={"red"} />
+        <hr />
+        {secondToggle && <MyComp color={"blue"} />}
+      </Box>
+    </div>
+  );
 }
 
 export default App;
